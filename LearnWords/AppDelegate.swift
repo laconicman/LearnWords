489fce4c2a0b32fbdf83e04174036b08e084d61a
Pav Tolmach <pav.tolmach@gmail.com>
2018-11-16 15:47:29 +0300

    Introduced customization and theme.

--- LearnWords/AppDelegate.swift
+++ LearnWords/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().setBackgroundImage(UIImage(named: "navigationBarBackground"), for: .default)
+        // Next lines should do the same, but they don't work for some reasons
+//        if let navigationBarBackground = UIImage(named: "navigationBarBackground") {
+//            UINavigationBar.appearance().backgroundColor = UIColor(patternImage: navigationBarBackground)
+        }
         return true
     }
 
